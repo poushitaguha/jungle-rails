d9b07c05bda6ee353cd499198af4ed31a6dc0281
poushitaguha <pguha.kolkata@gmail.com>
2019-07-11 20:31:07 +0000

    Enhance Order Page to display order details

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,16 @@
 class OrdersController < ApplicationController
 
+  before_filter :authorize
+  
   def show
     @order = Order.find(params[:id])
+    @line_items = LineItem.where(order_id: @order.id)
   end
+
+  def get_product_info product_id
+    Product.find_by(id: product_id)
+  end
+  helper_method :get_product_info  
 
   def create
     charge = perform_stripe_charge
