7c5cdaf75691737a5f5cd71d71b706181c9a5812
poushitaguha <pguha.kolkata@gmail.com>
2019-07-13 00:29:54 +0000

    Add review features step 11-13

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,33 @@
 class ReviewsController < ApplicationController
 
+    before_filter :authorize
+
     def create
-        raise "Yay, I'm here!"
-    end    
+        @product = Product.find params[:product_id]
+        @review = @product.reviews.new(review_params)
+        @review.user_id = current_user.id
+  
+        if @review.save
+          redirect_to @product  #, notice: "Your review has been successfully posted"
+        else
+          redirect_to @product  #, flash: { error: @review.errors.full_messages.first }
+        end      
+    end   
+    
+    def destroy
+        @review = Review.find params[:id]
+        @product_id = @review.product_id
+        @review.destroy
+        redirect_to Product.find @product_id
+    end      
+
+    private
+
+    def review_params
+        params.require(:review).permit(
+        :description,
+        :rating
+        )
+    end
 
 end
